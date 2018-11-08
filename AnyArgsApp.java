f3d1a4b63cac138857b9b1bc9db71620d1c0e60b
tipu <this.tipu@gmail.com>
2018-11-08 23:57:31 +0500

    AnyArgsTest

---
+++ AnyArgsApp.java
@@ -0,0 +1,8 @@
+/*This applicatin illustration the use of java cmd arguments*/
+public class AnyArgsApp{
+public static void main(String[] args){
+for (int i=0; i <args.length; i++) {
+	System.out.println(" Arguments: " +   i   + " value " +   args[i]);
+  } 
+ }//End main
+}//End class
